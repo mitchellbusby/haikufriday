4f6f831af17a289c3ffafc515a9674cf6027bfab
Mitchell Busby <mitchbusby@gmail.com>
2019-03-10 13:04:09 +1100

    Adds blah

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import {getHaiku} from './generateHaiku';
 
 const theHaiku = getHaiku();
 
-
 theHaiku.forEach((line, idx) => {
     const haikuContainer = document.getElementById(`haiku-container-${idx + 1}`);
     haikuContainer.textContent = line;
